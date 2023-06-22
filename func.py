18fb8c400cbd2641c6169e72ba6254012c005e2f
Oleg Diachenko <116761572+dichenk@users.noreply.github.com>
2023-06-22 21:34:02 +0300

    now it the program is waiting info about change of prices, counting it into percent and fixing in csv

--- func.py
+++ func.py
@@ -1,6 +1,30 @@
 import time as tm
 import pandas as pd
-import time
+
+
+def data_reset():
+    df = pd.DataFrame(columns=['time', 'price', 'prev_price', 'diff'])
+    future_data['ETHUSDT'] = df
+    future_data['BTCUSDT'] = df.copy()
+
+def analyse(var):
+    coin = var['data']['s']
+    price = var['data']['a']
+    time = var['data']['T']
+    
+    if price != last_price[coin]:
+        diff = float(price) / float(last_price[coin]) * 100 - 100  # in percent 
+
+        future_data[coin].loc[-1] = [time, price, last_price[coin], diff]
+        future_data[coin].index += 1
+        future_data[coin] = future_data[coin].sort_index()
+
+        future_data[coin].to_csv(f'{coin}.csv')
+
+        last_price[coin] = price
+        print(tm.strftime('%X'), coin, price, diff, 'процентов')
+    tm.sleep(1)
+    
 
 '''futures info'''
 future_data = {
@@ -9,34 +33,13 @@ future_data = {
     'BTCUSDT': None
     }
 streams = ['ethusdt@bookTicker', 'btcusdt@bookTicker']
-# coins = {
-#     'eth': 'ETHUSDT',
-#     'btc': 'BTCUSDT'
-#     }
-
-
-def data_reset():
-    df = pd.DataFrame(columns=['time', 'price'])
-    future_data['ETHUSDT'] = df
-    future_data['BTCUSDT'] = df.copy()
-
-def analyse(var):
-    data_reset()
-    coin = var['data']['s']
-    price = var['data']['a']
-    time = var['data']['T']
-
-    future_data[coin].loc[-1] = [time, price]
-    future_data[coin].index += 1
-    future_data[coin] = future_data[coin].sort_index()
-
-    tm.sleep(.5)
-    
-    # a = tm.strftime('%X')
-    # if a[3:5] == '02':
-    print(future_data)
-
-
+symbols = ['ETHUSDT', 'BTCUSDT']
+last_price = {
+    'ETHUSDT': -float('inf'),
+    'BTCUSDT': -float('inf')
+}
+data_reset()
 
 if __name__ == '__main__':
+    data_reset()
     analyse()
--- main.py
+++ main.py
@@ -11,11 +11,14 @@ def main():
     bsm = ThreadedWebsocketManager()
     bsm.start()
 
-    '''output to the trade_history'''
-    bsm.start_futures_multiplex_socket(callback=trade_history, streams=func.streams)
-
+    # '''output to the trade_history'''
+    # bsm.start_futures_multiplex_socket(callback=trade_history, streams=func.streams)
+    for i in func.symbols:
+        bsm.start_symbol_ticker_futures_socket(callback=trade_history, symbol=i)
+            
     '''
-    for keeping the ThreadedWebsocketManager running (join to the main thread)'''
+    for keeping the ThreadedWebsocketManager running (join to the main thread)
+    '''
     bsm.join()
 
 main()
