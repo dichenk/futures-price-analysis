48cc5abc6f3a178b20d87fd5a439b621ddec8779
Oleg Diachenko <116761572+dichenk@users.noreply.github.com>
2023-07-07 03:23:49 +0300

    script working. try to dockerize

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from binance import ThreadedWebsocketManager
 from threading import Thread
 import time as tm
-import pandas as pd
-import json
-
+import db_func
 
 class Stream:
 
@@ -20,29 +18,22 @@ class Stream:
 
         if self.prev_price[coin] is None:
             self.prev_price[coin] = price
+            db_func.write_data(coin, price, time_check, 0)
             return
 
         change_price = (price / self.prev_price[coin] - 1) * 100
 
-        if change_price:
-            print(coin, price, self.prev_price[coin], f'{change_price:.4f}')
+        if abs(change_price) > 0.001:
+            db_func.write_data(coin, price, time_check, change_price)
 
-        self.prev_price[coin] = price 
+        self.prev_price[coin] = price
 
-        #     '''add data to the df's up'''
-        #     future_data[coin].loc[-1] = {
-        #         'time_fix': time_check, 
-        #         'price': price, 
-        #         'prev_price': last_price[coin], 
-        #         'diff': diff
-        #     }
-        #     future_data[coin].index += 1
-        #     future_data[coin] = future_data[coin].sort_index()
-
-        #     last_price[coin] = price
-
-        #     future_data[coin].to_csv(f'{coin}.csv')
-
+        a = db_func.check_prices()
+        if int(tm.time()) % 10 == 0:
+            self.b = 1
+        if self.b == 1 and int(tm.time()) % 10 == 1: 
+            print(a[0] - a[1])
+            self.b = 0
 
     def start(self):
         self.bsm = ThreadedWebsocketManager()
@@ -54,6 +45,7 @@ class Stream:
             'BTCUSDT': None,
             'ETHUSDT': None
         }
+        self.b = 0  # variable for limit printing
 
         # output to the trade_history
         self.stream = self.bsm.start_futures_multiplex_socket(callback=stream.get_data, streams=self.streams)# symbol=self.symbols[0])
@@ -70,17 +62,7 @@ class Stream:
                 tm.sleep(5)
                 self.stream_error = False
                 self.stream = self.bsm.start_futures_multiplex_socket(callback=stream.get_data, streams=self.streams)#symbol=self.symbols[0])
-            
-
+ 
 stream = Stream()
 stream.start()
 stream.bsm.join()
-
-
-
-# '''futures info'''
-# df = pd.DataFrame(columns=['time_fix', 'price', 'prev_price', 'diff'])
-
-# future_data = dict()
-# future_data['BTCUSDT'] = df
-# future_data['ETHUSDT'] = df.copy()
